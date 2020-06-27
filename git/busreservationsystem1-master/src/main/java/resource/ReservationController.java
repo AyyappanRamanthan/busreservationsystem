ec80ecd71cbd5d926d13c0d02dc426f5b338829a
ayyappan gpr <ayyappangpr@mail.com>
2020-06-27 14:01:08 +0530

    controller update

--- git/busreservationsystem1-master/src/main/java/resource/ReservationController.java
+++ git/busreservationsystem1-master/src/main/java/resource/ReservationController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.annotations.Where;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,7 @@ public class ReservationController {
 		return reservationRepository.findAllBySourceCityAndDestCityAndTravelDate(sourceCity, destCity, travelDate);
 		//return null;
 	}
+	
+	
 
 }
